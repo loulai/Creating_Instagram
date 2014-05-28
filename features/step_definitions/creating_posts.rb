9204d89f7c1ca6e1e30dd1882a98aa6e2690eda0
Louise <louiselai88@gmail.com>
2014-05-28 17:33:04 +0100

    can upload image! all cucumber tests pass. Next step is to resize image

--- features/step_definitions/creating_posts.rb
+++ features/step_definitions/creating_posts.rb
@@ -4,7 +4,14 @@ When(/^I fill in the "(.*?)" area with "(.*?)"$/) do |area, stuff|
   fill_in(area, :with => stuff)
 end
 
+#-- photo stuff
+
 When(/^I attach a photo$/) do
   attach_file("Picture", Rails.root.join("spec/images/alex_in_action.png"))
 end
+
+Then(/^I should see my photo$/) do
+  expect(page).to have_css("img.uploaded-picture")
+end
+
 
