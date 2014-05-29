8e432fae36a0ea1c4a4b097eea176efc7e7df61e
Louise <louiselai88@gmail.com>
2014-05-29 16:45:48 +0100

    writing rspec test for one hashtag

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,27 @@
 require 'spec_helper'
-require 'factory_girl'
+
 
 describe Post do
 
 	let(:post) { create(:post) }
 
-  describe '#hashtag_names=' do
+  describe '#hashtag=' do
 
   	describe "no hashtags" do
-
   		it "does nothing" do
   		  post.hashtag = " "
   		  expect(post.hashtags).to be_empty
   		end
-  	  
   	end
+
+    describe "one hashtag" do
+      it "adds # when necessary" do
+        post.hashtag = "fluffy"
+        expect(post.hashtags.count).to eq 1
+        expect()    
+      end
+    end
+
   end
 end
 
